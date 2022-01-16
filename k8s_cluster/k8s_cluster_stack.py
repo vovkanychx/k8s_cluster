3ea2def9f506981fb821b47cf8104dc56b37d906
Vovkanych Dmytro <dima.vovkanych24@gmail.com>
2022-01-16 20:48:09 +0200

    commit

--- k8s_cluster/k8s_cluster_stack.py
+++ k8s_cluster/k8s_cluster_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_ec2 as ec2,
+    aws_eks as eks,
 )
 from constructs import Construct
 
@@ -10,10 +10,17 @@ class K8SClusterStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "K8SClusterQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        #create k8s cluster
+        k8s_cluster = eks.Cluster(self, "HelloEKS",
+            version=eks.KubernetesVersion.V1_21,
+            default_capacity=1
+        )
+        
+        #create managed node group comprised spot instance
+        k8s_cluster.add_nodegroup_capacity("managed-node-group",
+            instance_types=[
+                ec2.InstanceType("t2.micro")
+            ],
+            min_size=3,
+            capacity_type=eks.CapacityType.SPOT
+        )
